a392863db59ebf48fbfca8b3942b8a27e88e838e
Fres <richi181147@gmail.com>
2025-02-19 23:03:21 +0700

    update git ignore

--- rawdata/model_has_roles_202502192118.sql
+++ rawdata/model_has_roles_202502192118.sql
@@ -1,10 +1,10 @@
 INSERT INTO model_has_roles (role_id,model_type,model_id) VALUES
-	 (26,N'App\Models\User',1),
-	 (33,N'App\Models\User',14),
-	 (32,N'App\Models\User',15),
-	 (32,N'App\Models\User',16),
-	 (32,N'App\Models\User',17),
-	 (32,N'App\Models\User',23),
-	 (32,N'App\Models\User',24),
-	 (32,N'App\Models\User',25),
-	 (32,N'App\Models\User',26);
+	 ((SELECT id FROM roles WHERE id = 26),N'App\Models\User',1),
+	 ((SELECT id FROM roles WHERE id = 33),N'App\Models\User',14),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',15),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',16),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',17),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',23),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',24),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',25),
+	 ((SELECT id FROM roles WHERE id = 32),N'App\Models\User',26);
