4af021f39481983ddf1f9bc113c4fc2a609222ed
richi <richi181147@gmail.com>
2025-02-19 21:52:28 +0700

    backup image

---
+++ rawdata/products_202502192150.sql
@@ -0,0 +1,48 @@
+INSERT INTO products (id,product_name,sku,description,picture_path,purchase_price,selling_price,quantity,unit_of_measurement,created_by,updated_by,deleted_by,created_at,updated_at,deleted_at,is_included_in_receipt,[type],is_custom_price) VALUES
+	 (26,N'Fast Cut',NULL,NULL,NULL,NULL,66000,99731,NULL,1,14,NULL,'2024-09-22 16:30:52.02','2025-02-19 20:15:21.653',NULL,1,N'Service',0),
+	 (27,N'Hair Cut and Wash',NULL,NULL,NULL,NULL,76000,999913,NULL,1,14,NULL,'2024-09-22 16:31:31.503','2025-02-17 19:40:47.34',NULL,1,N'Service',0),
+	 (28,N'Face Mask',NULL,NULL,NULL,NULL,60000,9999993,NULL,1,1,NULL,'2024-09-22 16:31:55.887','2024-10-02 00:23:20.07',NULL,1,NULL,0),
+	 (29,N'Relaxation Spa',NULL,NULL,NULL,NULL,60000,9999999,NULL,1,1,NULL,'2024-09-22 16:32:17.397','2024-09-22 16:32:17.397',NULL,1,NULL,0),
+	 (30,N'Dark Classic Hair Color (S)',NULL,NULL,NULL,NULL,150000,9999989,NULL,1,1,NULL,'2024-09-22 16:33:09.393','2025-01-04 21:24:21.183',NULL,1,NULL,0),
+	 (31,N'Dark Classic Hair Color (M)',NULL,NULL,NULL,NULL,250000,9999984,NULL,1,1,NULL,'2024-09-22 16:33:34.213','2024-09-29 21:23:16.79',NULL,1,NULL,0),
+	 (32,N'Premium Grooming 1',NULL,NULL,NULL,NULL,110000,9999914,NULL,1,1,NULL,'2024-09-22 16:34:04.3','2025-02-19 17:46:36.56',NULL,1,NULL,0),
+	 (33,N'Premium Grooming 2',NULL,NULL,NULL,NULL,90000,9999932,NULL,1,1,NULL,'2024-09-22 16:34:19.807','2025-02-10 19:00:35.323',NULL,1,NULL,0),
+	 (34,N'Fashion Grooming (S)',NULL,NULL,NULL,NULL,210000,9999999,NULL,1,1,NULL,'2024-09-22 16:34:43.007','2024-09-22 16:34:43.007',NULL,1,NULL,0),
+	 (35,N'Fashion Grooming (M)',NULL,NULL,NULL,NULL,310000,9999985,NULL,1,1,NULL,'2024-09-22 16:35:11.717','2024-10-24 20:24:05.237',NULL,1,NULL,0);
+INSERT INTO products (id,product_name,sku,description,picture_path,purchase_price,selling_price,quantity,unit_of_measurement,created_by,updated_by,deleted_by,created_at,updated_at,deleted_at,is_included_in_receipt,[type],is_custom_price) VALUES
+	 (36,N'Health Scalp & Grooming',NULL,NULL,NULL,NULL,210000,9999798,NULL,1,1,NULL,'2024-09-22 16:37:08.783','2024-10-24 20:26:31.243',NULL,1,N'Retail',0),
+	 (40,N'Classic Grooming (S)',NULL,NULL,N'files/products/1732160292.jpg',0,210000,9999984,NULL,1,14,NULL,'2024-09-22 16:37:08.783','2024-11-21 16:34:51.57',NULL,1,N'Service',0),
+	 (41,N'Cutting Bayar Seikhlasnya',NULL,N'Berlaku s/d 31 Dec 24',NULL,NULL,1,9968999,NULL,14,1,1,'2024-11-21 11:25:26.137','2025-01-04 12:40:29.44','2025-01-04 12:40:29.44',0,N'Service',0),
+	 (42,N'Add Washing',NULL,NULL,N'files/products/1732165441.jpg',NULL,10000,999697,NULL,14,1,1,'2024-11-21 12:04:02.127','2025-01-04 12:41:10.6','2025-01-04 12:41:10.6',1,N'Retail',0),
+	 (43,N'Cutting Bayar Seikhlasnya 2',NULL,NULL,NULL,0,1000,0,NULL,1,1,1,'2024-11-22 20:07:33.627','2025-01-04 12:40:18.49','2025-01-04 12:40:18.49',0,N'Service',0),
+	 (44,N'Cutting Bayar Seikhlasnya 2',NULL,NULL,NULL,0,1000,999999999,NULL,1,1,1,'2024-11-22 20:07:34.153','2024-11-22 20:07:54.803','2024-11-22 20:07:54.803',0,N'Service',0),
+	 (45,N'ikhlas coba',NULL,NULL,NULL,NULL,0,55,NULL,1,1,1,'2024-12-03 21:23:58.427','2024-12-03 21:24:54.547','2024-12-03 21:24:54.547',0,N'Retail',1),
+	 (46,N'Kahf Volumized Suave Hair Powder 10g',NULL,NULL,N'files/products/1737711499.jpg',NULL,79000,5,NULL,1,1,NULL,'2025-01-24 16:38:19.207','2025-02-15 15:12:02.32',NULL,0,N'Retail',0),
+	 (47,N'Kahf Malte Dapper Water-based pomade 70g',NULL,NULL,N'files/products/1737711597.jpg',NULL,45000,3,NULL,1,1,NULL,'2025-01-24 16:39:57.127','2025-01-24 16:39:57.127',NULL,0,N'Retail',0),
+	 (48,N'Kahf Sleek Classy Water-based Pomade 70g',NULL,NULL,N'files/products/1737711685.jpg',NULL,45000,1,NULL,1,1,NULL,'2025-01-24 16:41:25.937','2025-02-17 13:40:27.75',NULL,0,N'Retail',0);
+INSERT INTO products (id,product_name,sku,description,picture_path,purchase_price,selling_price,quantity,unit_of_measurement,created_by,updated_by,deleted_by,created_at,updated_at,deleted_at,is_included_in_receipt,[type],is_custom_price) VALUES
+	 (49,N'Kahf Acne Pore Cleanse Face Wash 100 ml',NULL,NULL,N'files/products/1737711852.jpg',NULL,48000,3,NULL,1,1,NULL,'2025-01-24 16:44:12.403','2025-01-24 16:44:12.403',NULL,0,N'Retail',0),
+	 (50,N'Kahf Extra Dry Antiperspirant Deodorant Roll On 45 ml',NULL,NULL,N'files/products/1737711929.jpg',NULL,29000,3,NULL,1,1,NULL,'2025-01-24 16:45:29.307','2025-01-24 16:45:29.307',NULL,0,N'Retail',0),
+	 (51,N'Kahf Cooling Power Antiperspirant Deodorant Roll On 45 ml',NULL,NULL,N'files/products/1737712020.jpg',NULL,25000,3,NULL,1,1,NULL,'2025-01-24 16:47:00.523','2025-01-24 16:47:00.523',NULL,0,N'Retail',0),
+	 (52,N'Kahf Clean Fresh Antiperspirant Deodorant Roll On 45 ml',NULL,NULL,NULL,NULL,25000,3,NULL,1,1,NULL,'2025-01-24 16:49:15.097','2025-01-24 16:49:15.097',NULL,0,N'Retail',0),
+	 (53,N'Kahf Invisible Matte Sunscreen Stich 22 G',NULL,NULL,NULL,NULL,125000,3,NULL,1,1,NULL,'2025-01-24 16:55:48.813','2025-01-24 16:55:48.813',NULL,0,N'Retail',0),
+	 (54,N'Kahf Oil and Acne Care Face \Yash 50 ml',NULL,NULL,NULL,NULL,32000,3,NULL,1,1,NULL,'2025-01-24 16:56:10.633','2025-01-24 16:56:10.633',NULL,0,N'Retail',0),
+	 (55,N'Kahf Skin Energising and brightening Face Wash 50 ml',NULL,NULL,NULL,NULL,32000,3,NULL,1,1,NULL,'2025-01-24 16:56:34.383','2025-01-24 16:56:34.383',NULL,0,N'Retail',0),
+	 (56,N'Kahf All-in-One Power Toning Face Spray 100 ml',NULL,NULL,NULL,NULL,85000,3,NULL,1,1,NULL,'2025-01-24 16:56:58.65','2025-01-24 16:56:58.65',NULL,0,N'Retail',0),
+	 (57,N'Kahf Brightening and Texture Refining Face Serum 35 ml',NULL,NULL,NULL,NULL,112000,3,NULL,1,1,NULL,'2025-01-24 16:57:42.907','2025-01-24 16:57:42.907',NULL,0,N'Retail',0),
+	 (58,N'Kahf Saffron Oud Eau de Parfum 100 ml',NULL,NULL,NULL,NULL,287000,3,NULL,1,1,NULL,'2025-01-24 16:58:10.283','2025-01-24 16:58:10.283',NULL,0,N'Retail',0);
+INSERT INTO products (id,product_name,sku,description,picture_path,purchase_price,selling_price,quantity,unit_of_measurement,created_by,updated_by,deleted_by,created_at,updated_at,deleted_at,is_included_in_receipt,[type],is_custom_price) VALUES
+	 (59,N'Kahf Revered Oud Eau de Parfum 100 ml',NULL,NULL,NULL,NULL,287000,3,NULL,1,1,NULL,'2025-01-24 16:58:35.003','2025-01-24 16:58:35.003',NULL,0,N'Retail',0),
+	 (60,N'Kahf Brightening and Cooling Body Wash 200 ml',NULL,NULL,NULL,NULL,32000,3,NULL,1,1,NULL,'2025-01-24 16:58:59.467','2025-01-24 16:58:59.467',NULL,0,N'Retail',0),
+	 (61,N'Kahf Acne and Fight Relaxing Body Wash 200 mI',NULL,NULL,NULL,NULL,32000,3,NULL,1,1,NULL,'2025-01-24 16:59:21.647','2025-01-24 16:59:21.647',NULL,0,N'Retail',0),
+	 (62,N'Kahf Triple + Protection Sunscreen kloisturizer 30 ml',NULL,NULL,NULL,NULL,48000,3,NULL,1,1,NULL,'2025-01-24 16:59:46.55','2025-01-24 16:59:46.55',NULL,0,N'Retail',0),
+	 (63,N'Kahf Triple Action Oil and Come do Defense Face Wash 100 ml',NULL,NULL,NULL,NULL,50000,3,NULL,1,1,NULL,'2025-01-24 17:00:08.407','2025-01-24 17:00:08.407',NULL,0,N'Retail',0),
+	 (64,N'Kahf Gentle Exfoliating face scrub 100 ml',NULL,NULL,NULL,NULL,48000,3,NULL,1,1,NULL,'2025-01-24 17:00:30.943','2025-01-24 17:00:30.943',NULL,0,N'Retail',0),
+	 (65,N'Kahf Skin Energizing and Brightening Face Wash 100 ml',NULL,NULL,NULL,NULL,48000,3,NULL,1,1,NULL,'2025-01-24 17:08:05.297','2025-01-24 17:08:05.297',NULL,0,N'Retail',0),
+	 (66,N'Kahf Oil and Acne Care Face v/ash 100 ml',NULL,NULL,NULL,NULL,48000,2,NULL,1,1,NULL,'2025-01-24 17:09:35.177','2025-02-14 11:34:23.31',NULL,0,N'Retail',0),
+	 (67,N'Kahf True Broltierhood Eau De Toilette 35 ml',NULL,NULL,NULL,NULL,96000,3,NULL,1,1,NULL,'2025-01-24 17:10:00.747','2025-01-24 17:10:00.747',NULL,0,N'Retail',0),
+	 (68,N'Kahf Revered Oud Eau De Toilette 35 ml',NULL,NULL,NULL,NULL,96000,3,NULL,1,1,NULL,'2025-01-24 17:10:24.983','2025-01-24 17:10:24.983',NULL,0,N'Retail',0);
+INSERT INTO products (id,product_name,sku,description,picture_path,purchase_price,selling_price,quantity,unit_of_measurement,created_by,updated_by,deleted_by,created_at,updated_at,deleted_at,is_included_in_receipt,[type],is_custom_price) VALUES
+	 (69,N'Kahf Invigorating Waterfall Eau De Toilelte 35 ml',NULL,NULL,NULL,NULL,96000,3,NULL,1,1,NULL,'2025-01-24 17:17:29.02','2025-01-24 17:17:29.02',NULL,0,N'Retail',0),
+	 (70,N'Kahf Humbling Foresl Eau De Toilette 35 ml',NULL,NULL,NULL,NULL,96000,3,NULL,1,1,NULL,'2025-01-24 17:17:54.22','2025-01-24 17:17:54.22',NULL,0,N'Retail',0),
+	 (71,N'Kahf Nourshing Beard and Hair Oil 30mI',NULL,NULL,NULL,NULL,125000,1,NULL,1,1,NULL,'2025-02-17 18:53:50.193','2025-02-17 20:32:17.217',NULL,0,N'Retail',0);
